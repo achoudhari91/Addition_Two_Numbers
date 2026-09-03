f29301ad68b367df7a5155db224be9c6d0dbd8a5
Ajit Choudhari <ajitchoudhari2006@gmail.com>
2026-09-03 23:23:39 +0530

    Create Addition.cpp

---
+++ Addition.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int a, b, sum;
+
+    cout << "Enter First Integer: "<<endl;
+    cin >> a;
+
+    cout << "Enter Second Integer: "<<endl;
+    cin >> b;
+
+    sum = a + b;
+
+    cout << "Sum = " << sum;
+
+    return 0;
+}
